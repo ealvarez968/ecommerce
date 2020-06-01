5a8ca55bdbfa8c13c65465fdeacff79f7da1d454
Eduardo Alvarez <ealvarez0896@gmail.com>
2020-05-31 20:26:44 -0600

    Remove Jupiter Junit dependency

--- src/test/java/com/example/demo/SareetaApplicationTests.java
+++ src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.jupiter.api.Assertions;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.transaction.annotation.Transactional;
